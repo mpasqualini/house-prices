87a91f28b1a1ca2ac02997ea077fdf913f4cc7f9
mpasqualini <pasqualinimari@gmail.com>
2019-12-25 23:44:16 -0300

    atualização com correlation matrix e inicio do tratamento de NAs

--- house_prices.R
+++ house_prices.R
@@ -1,6 +1,9 @@
 library(dplyr)
+library(tidyr)
 library(ggplot2)
+library(ggcorrplot)
 library(scales)
+library(FactoMineR)
 
 # Leitura dos arquivos----
 train <- read.csv(file = "./data/train.csv", stringsAsFactors = FALSE)
@@ -8,6 +11,9 @@ test <- read.csv(file = "./data/test.csv", stringsAsFactors = FALSE)
 
 dim(train) # dimensao do conjunto de treino
 str(train) # estrutura do conjunto de treino
+
+train <- train %>% 
+          select(-Id)
 
 # Analise exploratoria----
 train %>% 
@@ -28,9 +34,24 @@ train %>%
   ggplot(aes(x = as.factor(Neighborhood))) +
     geom_bar()
 
-cor(train$SalePrice, train$LotArea)
-cor(train$SalePrice, train$X1stFlrSF)
-cor(train$SalePrice, train$X2ndFlrSF)
-cor(train$SalePrice, train$GarageCars)
-cor(train$SalePrice, train$GarageArea)
-cor(train$SalePrice, train$TotalBsmtSF)
+correl <- train %>% 
+            select_if(is.numeric) %>% 
+              cor()
+
+ggcorrplot(correl, type="lower")
+
+# Inputation----
+
+train <- train %>% 
+          mutate(PoolQC = case_when(is.na(PoolQC) ~ "No pool"),
+                 MiscFeature = case_when(is.na(MiscFeature) ~ "None"),
+                 Fence = case_when(is.na(Fence) ~ "No fence"),
+                 FireplaceQu = case_when(is.na(FireplaceQu) ~ "No fireplace"),
+                 Alley = case_when(is.na(Alley) ~ "No alley access"))
+
+train <- train %>% 
+  mutate(PoolQC = ifelse(is.na(PoolQC), "No pool", PoolQC),
+         MiscFeature = ifelse(is.na(MiscFeature), "None", MiscFeature),
+         Fence = ifelse(is.na(Fence), "No fence", Fence),
+         FireplaceQu = ifelse(is.na(FireplaceQu), "No fireplace", FireplaceQu),
+         Alley = ifelse(is.na(Alley), "No alley access", Alley))
